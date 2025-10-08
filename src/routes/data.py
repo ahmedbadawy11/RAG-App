7724705fa11a04e28111d123b40795059065a8c3
Ahmed Badawy <59053820+ahmedbadawy11@users.noreply.github.com>
2025-10-09 01:25:20 +0300

    process files

--- src/routes/data.py
+++ src/routes/data.py
@@ -3,9 +3,11 @@ from fastapi.responses import JSONResponse
 import os
 import aiofiles
 from helpers.config import get_settings,settings
-from controllers import DataController,ProjectController
+from controllers import DataController,ProjectController,ProcesController
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
+
 
 logger=logging.getLogger('uvicorn.error')
 
@@ -53,3 +55,33 @@ async def upload_data(project_id: str, file: UploadFile,
 
             }
     )
+
+
+
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request:ProcessRequest,
+                     ):
+    
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller=ProcesController(project_id=project_id)
+
+    file_content=process_controller.get_file_content(file_id=file_id)
+    
+    file_chunks=process_controller.process_file_content(file_content=file_content,
+                                                  file_id=file_id,
+                                                    chaunk_size=chunk_size,
+                                                    overlap_size=overlap_size)
+    
+    if file_chunks is None or len(file_chunks)==0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "Signal": ResponseSignal.FILE_PROCESSING_FAILED.value,
+                "file_id": file_id
+            }
+        )
+    return file_chunks
---
+++ src/routes/schemes/data.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+
+class ProcessRequest(BaseModel):
+    file_id: str 
+    chunk_size: Optional[int] = 100
+    overlap_size: Optional[int] = 20
+    do_reset: Optional[int] = 0
+    
+
